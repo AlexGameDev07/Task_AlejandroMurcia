16a785e5da521cfbc3f30d60d7894bc551eb9dcb
AlexDev <alekeymurcia@gmail.com>
2025-06-16 15:12:08 -0600

    Agregué documentación

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 import express from "express";
 import taskRouter from "./src/routes/taskRoutes.js";
 
+//parte de la documentación
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./docs.json"), 'utf8')
+
+)
+
 const app = express();
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
